99d3e6444f0fc1f7637fa8a719ad65c5c5c29a3c
LanzeG <119438401+LanzeG@users.noreply.github.com>
2024-02-09 02:49:43 +0800

    Add files via upload

---
+++ main.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Home Page</title>
+<!-- ICONSCOUT CDN -->
+<link rel="stylesheet" href="https://unicons.iconscout.com/release/v2.1.6/css/unicons.css">
+<!-- STYLESHEET -->
+<link rel="stylesheet" href="style1.css">
+</head>
+<body>
+<nav>
+<div class="container">
+<h2 class="log">
+Lanze Development
+</h2>
+<div class="search-bar">
+<i class="uil uil-search"></i>
+<input type="search" placeholder="Search">
+</div>
+<div class="create">
+<label class="btn btn-primary" for="create-post">Create</label>
+<div class="profile-photo">
+<img src="https://cdn.vectorstock.com/i/preview-1x/10/00/artwork-monogram-l-letter-calligraphy-logo-vector-46601000.jpg">
+</div>
+</div>
+</div>
+</nav>
+ <!----------------------------------MAIN-->
+<main>
+    <div class="container">
+<!-------------------------LEFT-->
+        <div class="left">
+            <a class="profile">
+                <div class="profile-photo">
+                    <img src="https://scontent-mnl1-1.xx.fbcdn.net/v/t39.30808-6/355886242_2118704264993603_1509992936144839988_n.jpg?stp=cp6_dst-jpg&_nc_cat=108&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFVU7cIX1EwpHJF3jjSeAdL0vQ900Ys2R_S9D3TRizZH8sKpQTjnrnX_B7NxV7FRzrtW9-c2i4cgfBipM2jcOz6&_nc_ohc=KobaMP3x7CQAX-lZVQV&_nc_ht=scontent-mnl1-1.xx&oh=00_AfB9Eg3AKzlUbbpxaVzwxLJby9D8nUHxTAeZMqJydxTHCA&oe=649DA028">
+                </div>
+                <div class="handle">
+                    <h4>LanzeG</h4>
+                    <p class="text-muted">
+                        @lanze
+                    </p>
+                </div>
+            </a>
+    <!--------------------------SIDEBAR--------->
+    <div class="sidebar">
+        <a class="menu-item active">
+            <span><i class="uil uil-home"></i></span><h3>Home</h3>
+        </a>
+        <a class="menu-item">
+            <span><i class="uil uil-compass"></i></span><h3>Explore</h3>
+        </a>
+        <a class="menu-item" id="notifications">
+              <span><i class="uil uil-bell"><small class="Notificaton-count">9+</small></i></span><h3>Notifications</h3>
+    <!---------------------NOTIFICATION POPUP-------->
+    <!-- <div class="notification popup">
+        <div>
+            <div class="profile-photo">
+                <img src="luffy.jpg">
+            </div>
+            <div class="Notification-body">
+                <b>Luffy</b> accepted your friend request
+                <small class="text-muted">2 DAYS AGO</small>
+            </div>
+        </div>
+        <div>
+            <div class="profile-photo">
+                <img src="denji.jpg">
+            </div>
+            <div class="Notification-body">
+                <b>Denji</b> Commented on your post
+                <small class="text-muted">10 MINS AGO</small>
+            </div>
+        </div>
+        <div>
+            <div class="profile-photo">
+                <img src="gojo.jpg">
+            </div>
+            <div class="Notification-body">
+                <b>Gojo</b> and <b>283 others</b> liked your post
+                <small class="text-muted">3 DAYS AGO</small>
+            </div>
+        </div>
+        <div>
+            <div class="profile-photo">
+                <img src="hinata.jpg">
+            </div>
+            <div class="Notification-body">
+                <b>Hinata</b> Commented on post you are tagged in 
+                <small class="text-muted">15 MINS AGO</small>
+            </div>
+        </div>
+        <div>
+            <div class="profile-photo">
+                <img src="eren.png">
+            </div>
+            <div class="Notification-body">
+                <b>Eren</b> started following you
+                <small class="text-muted">4 DAYS AGO</small>
+            </div>
+        </div> 
+    </div> -->
+        </a>
+        <a class="menu-item" id="Message-notifications">
+            <span><i class="uil uil-envelope-alt"><small class="notification-count">6</small></i></span><h3>Message</h3>
+        </a>
+        <a class="menu-item">
+            <span><i class="uil uil-bookmark"></i></span><h3>Bookmarks</h3>
+        </a>
+        <a class="menu-item">
+            <span><i class="uil uil-chart-line"></i></span><h3>Analytics</h3>
+        </a>
+        <a class="menu-item">
+            <span><i class="uil uil-palette"></i></span><h3>Theme</h3>
+        </a>
+        <a class="menu-item">
+            <span><i class="uil uil-setting"></i></span><h3>Settings</h3>
+        </a>
+    </div>
+        <label for="create post" class="btn btn-primary">Create Post</label>
+      </div>
+<!-------------------------------------MIDDLE-->
+        <!-- <div class="middle">
+            There are many different ways to organize a paragraph.
+             The organization you choose will depend on the controlling idea of the paragraph.
+              Below are a few possibilities for organization, with links to brief examples:
+Narration: Tell a story. Go chronologically, 
+from start to finish. (See an example.)
+Description: Provide specific details about what something looks,
+ smells, tastes, sounds, or feels like. Organize spatially
+ , in order of appearance, or by topic. (See an example.)
+Process: Explain how something works, step by step.
+ Perhaps follow a sequence—first, second, third. 
+ (See an example.)
+Classification: Separate into groups or explain the various parts of a topic.
+ (See an example.)
+Illustration: Give examples and explain how those examples support your point.
+ (See an example in the 5-step process below.)
+        </div> -->
+ <!------------------RIGHT-->
+        <!-- <div class="right">
+            The next movement in paragraph development is an explanation
+            0 of each example and its relevance to the topic sentence.
+             The explanation should demonstrate the value of the example
+              as evidence to support the major claim, or focus, in your paragraph.
+Continue the pattern of giving examples and explaining 
+them until all points/examples that the writer deems necessary
+ have been made and explained. NONE of your examples should be left unexplained.
+  You might be able to explain the relationship between the example and
+   the topic sentence in the same sentence which introduced the example. M
+   ore often, however, you will need to explain that relationship in a separate sentence.
+Explanation for example — Such a terrifying representation easily captures the
+ imagination and promotes unnecessary fear.
+Notice that the example and explanation steps of this 5-step process
+ (steps 3 and 4) can be repeated as needed. The idea is that you continue 
+ to use this pattern until you have completely developed the main idea of the paragraph.
+        </div> -->
+     </div>
+</main> 
+
+</body>
+<script type="module">
+    // Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-auth.js";
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";.
+    // TODO: Add SDKs for Firebase products that you want to use
+    // https://firebase.google.com/docs/web/setup#available-libraries
+  
+    // Your web app's Firebase configuration
+   
+  </script>
+<script src="main.js"></script>
+</html>
